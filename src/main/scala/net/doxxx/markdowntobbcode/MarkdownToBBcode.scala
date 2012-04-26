d514eb4999c5c3f4c32ae421709b3e7bcc74b5a9
Gordon Tyler <gordon@doxxx.net>
2012-04-26 15:14:11 -0400

    Ensure that files are clsoed properly.

--- src/main/scala/net/doxxx/markdowntobbcode/MarkdownToBBcode.scala
+++ src/main/scala/net/doxxx/markdowntobbcode/MarkdownToBBcode.scala
@@ -10,10 +10,19 @@ import java.io.FileWriter
  */
 object MarkdownToBBcode extends App {
   val file = Source.fromFile(args(0))
-  val processor = new PegDownProcessor()
-  val root = processor.parseMarkdown(file.toArray)
-  val generator = new BBcodeGenerator
-  val writer = new FileWriter("out.txt")
-  writer.write(generator.toBBcode(root))
-  writer.close()
+  try {
+    val processor = new PegDownProcessor()
+    val root = processor.parseMarkdown(file.toArray)
+    val generator = new BBcodeGenerator
+    val writer = new FileWriter("out.txt")
+    try {
+      writer.write(generator.toBBcode(root))
+    }
+    finally {
+      writer.close()
+    }
+  }
+  finally {
+    file.close()
+  }
 }

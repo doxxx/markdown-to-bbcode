248c19ab49c3a3f9f4926200efc977f74476f3c1
Gordon Tyler <gordon@doxxx.net>
2012-04-26 19:31:10 -0400

    Imrpoved command-line args handling.

--- src/main/scala/net/doxxx/markdowntobbcode/MarkdownToBBcode.scala
+++ src/main/scala/net/doxxx/markdowntobbcode/MarkdownToBBcode.scala
@@ -2,19 +2,25 @@ package net.doxxx.markdowntobbcode
 
 import org.pegdown.PegDownProcessor
 import io.Source
-import java.io.FileWriter
+import java.io.{OutputStreamWriter, FileWriter}
 
 /**
  * MarkdownToBBcode app.
  * @author Gordon Tyler
  */
 object MarkdownToBBcode extends App {
+  if (args.isEmpty) {
+    Console.println("Usage: MarkdownToBBcode <filename> [<outputfilename>]")
+    System.exit(255)
+  }
   val file = Source.fromFile(args(0))
   try {
     val processor = new PegDownProcessor()
     val root = processor.parseMarkdown(file.toArray)
     val generator = new BBcodeGenerator
-    val writer = new FileWriter("out.txt")
+    val writer =
+      if (args.size == 2) new FileWriter(args(1))
+      else new OutputStreamWriter(System.out)
     try {
       writer.write(generator.toBBcode(root))
     }
